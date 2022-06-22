4b4917c0d83b19cca241d2b7d935ba07dac987f6
stuutz <32516714+stuutz@users.noreply.github.com>
2022-06-22 07:22:34 -0400

    Add files via upload

---
+++ Install_ConnectFonts-dmg.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+####################################################################################################
+#
+# CREATED BY
+#
+#   Brian Stutzman
+#
+# DESCRIPTION
+#
+#	This script installs the latest version of Connect Fonts.
+#
+#	File type: dmg
+#
+# VERSION
+#
+#	- 1.0
+#
+# CHANGE HISTORY
+#
+# 	- Created script - 6/13/2022 (1.0)
+#
+####################################################################################################
+
+
+##########################################################################################
+# Script variables (edit below)
+##########################################################################################
+AppName="Connect Fonts"
+dmgApp="/Volumes/$AppName/$AppName.app"
+dmgMountName="/Volumes/$AppName"
+dmgFilePath="/private/tmp/$AppName.dmg"
+localAppPath="/Applications/$AppName.app"
+
+# Identify chip architecture
+arch=$(/usr/bin/arch)
+if [ "$arch" == "arm64" ]; then
+    echo "Apple Silicon Found"
+    
+	DownloadLink="https://links.extensis.com/suitcase/sf_latest?language=en&platform=mac"
+          
+elif [ "$arch" == "i386" ]; then
+    
+    echo "Intel Found"
+    
+    DownloadLink="https://links.extensis.com/suitcase/sf_latest?language=en&platform=mac"
+    
+else
+    
+    echo "Unknown Architecture"
+    
+	DownloadLink="https://links.extensis.com/suitcase/sf_latest?language=en&platform=mac"
+    
+fi
+
+##########################################################################################
+
+
+##########################################################################################
+# DO NOT EDIT ANYTHING BELOW
+##########################################################################################
+
+
+##########################################################################################
+# Download/install app and change permissions
+##########################################################################################
+function DownloadApp () 
+{
+    
+    # Download the latest version of the app
+	curl -L -o "$dmgFilePath" "$DownloadLink"
+    
+    echo "Script result: Downloaded app"
+
+	RemoveApp
+
+}
+
+##########################################################################################
+# Remove the installed version of the app
+##########################################################################################
+function RemoveApp () 
+{
+
+    if [ -d "$localAppPath" ]; then
+    
+        # Kill the app process
+		pkill -9 "$AppName"
+    
+    	sleep 3
+    
+        # Delete app
+		rm -rf "$localAppPath"
+        
+		sleep 3
+        
+		echo "Script result: Deleted old app version"
+
+	else
+
+    	echo "Script result: App not found"
+        
+    fi
+    
+    CopyApp
+
+}
+
+##########################################################################################
+# Copy app and change permissions
+##########################################################################################
+function CopyApp () 
+{
+
+	# Mount the dmg file
+	hdiutil attach -nobrowse "$dmgFilePath"
+
+    sleep 5
+
+    # Copy new app to the applications folder
+	sudo cp -R "$dmgApp" /Applications
+
+    # Setting app permissions and owner
+	chown -R root:wheel "$localAppPath"
+	chmod -R 755 "$localAppPath"
+
+    echo "Script result: Copied new app to Applications folder"
+
+	CleanUp
+
+}
+
+##########################################################################################
+# Remove files in the /private/tmp folder
+##########################################################################################
+function CleanUp () 
+{
+
+    # Un-mount/eject the dmg file
+    diskutil eject "$dmgMountName"
+    
+    sleep 3
+
+	# Delete dmg file
+   	if [[ -e "$dmgFilePath" ]]; then
+
+    	rm -rf "$dmgFilePath"
+        
+    fi
+    
+    echo "Script result: Cleanup completed"
+
+}
+
+##########################################################################################
+# Main script
+##########################################################################################
+
+DownloadApp
+
+exit 0
